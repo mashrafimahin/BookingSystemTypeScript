0597ae99e34d99ccc4fe86a64f3e68480c8c2bfa
Mahin <mashrafi.devs@gmail.com>
2026-03-31 11:59:35 +0600

    Style with Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,17 @@
+// Image
+import bg from "./assets/bg.jpg";
+// component
+import Form from "./component/Form";
+// main
 function App() {
-  return "Hello World";
+  return (
+    <div
+      className="px-4 md:px-20 py-6 w-full flex bg-cover bg-center bg-gray-300 bg-blend-multiply"
+      style={{ backgroundImage: `url(${bg})` }}
+    >
+      <Form />
+    </div>
+  );
 }
 
 export default App;
